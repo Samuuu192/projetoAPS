9264c69af3866c324b51b17fb90c0132a3851458
Sabasi <66352539+Samuuu192@users.noreply.github.com>
2022-12-06 14:54:28 -0300

    endereÃ?o

--- projetoaps/src/com/ifsc/tds/main/Cliente.java
+++ projetoaps/src/com/ifsc/tds/main/Cliente.java
@@ -2,4 +2,19 @@ package com.ifsc.tds.main;
 
 public class Cliente {
 
+  int clienteId;
+  String nome;
+  Endereco endereco;
+  
+  int getClienteId(){return this.clienteId}
+  void setClienteId(int id){this.clienteId = id}
+  
+  String getNome(){return this.nome}
+  void setNome(String nome){this.nome = nome}
+  
+  Endereco getEndereco(){return this.endereco}
+  void setEndereco(Endereco endereco){this.endereco = endereco}
+  
+  String toString(){
+  }
 }
